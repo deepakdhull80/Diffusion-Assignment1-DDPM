6b43e727768a089f351bd124059382c88f50ed85
deepak.dhull <deepak.dhull@verse.in>
2025-03-16 11:06:11 +0530

    change batch size

--- image_diffusion_todo/train.py
+++ image_diffusion_todo/train.py
@@ -126,7 +126,7 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--gpu", type=int, default=0)
-    parser.add_argument("--batch_size", type=int, default=64)
+    parser.add_argument("--batch_size", type=int, default=32)
     parser.add_argument(
         "--train_num_steps",
         type=int,
